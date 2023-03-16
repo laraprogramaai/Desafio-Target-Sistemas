a10a5c0f51197d34792577bb607e8dce1282cb1c
Lara Magalhães Tagliani <laragliani.maga27@gmail.com>
2023-03-16 14:12:58 -0300

    Respostas do teste
    Adicionei as respostas do teste para o processo seletivo.

---
+++ src/js/teste2.js
@@ -0,0 +1,11 @@
+const fibonacci = n => {
+    let a = 0, b = 1, c = n;
+    
+    for(let i = 2; i <= n; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    
+    return c;
+  };
---
+++ src/js/teste4.js
@@ -0,0 +1,23 @@
+const billings = [
+    {
+      name: 'SP',
+      value: 67836.43
+    },
+    {
+      name: 'RJ',
+      value: 36678.66
+    },
+    {
+      name: 'MG',
+      value: 29229.88
+    },
+    {
+      name: 'ES',
+      value: 27165.48
+    },
+    {
+      name: 'Outros',
+      value: 19849.53
+    },
+  
+  ]
